dc5fb1b5cf91bda4f3903752b84797e570069c18
Ailton Luiz <ailtonatn@gmail.com>
2021-04-29 13:50:35 +0200

    Implementando controllers

--- src/main/java/com/ailtonluiz/sgdapi/SgdApiApplication.java
+++ src/main/java/com/ailtonluiz/sgdapi/SgdApiApplication.java
@@ -2,12 +2,17 @@ package com.ailtonluiz.sgdapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class SgdApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SgdApiApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
 
 }
